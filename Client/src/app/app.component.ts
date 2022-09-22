00d9ef7aa44a499c3a1205924d2bed0311b86943
Kyle McCullen <kmccullen@protonmail.com>
2022-09-22 13:34:02 -0400

    move styling to styles.css

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { MessagesService } from 'src/services/messages.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   name: string = '';
